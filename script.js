626aa92686ac39c792e408a407499703637227c6
rozo97 <131261797+rozo97@users.noreply.github.com>
2025-09-05 17:11:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function contactDiscord(botName) {
+  alert(`Je hebt gekozen voor: ${botName}\n\nNeem contact op via Discord: jouwDiscordNaam#1234`);
+  // Later kan je hier een echte bestelpagina of DM-link maken
+}
